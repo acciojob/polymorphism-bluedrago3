6fb44741c451bdf7f54211d182642db2f4f164f3
bluedrago3 <95340265+bluedrago3@users.noreply.github.com>
2023-02-15 14:41:45 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,25 +1,44 @@
 package com.driver;
 
 public class Main {
-  class product{
+  class roduct{
     int typesOffrutis;
     int numofdefectivefruits;
     int numofdefItem;
     
-     public int product(int x, int y) {
+     public int product(int x, int y) 
        {
-         typesoffruits=x;
+         typesOffrutis=x;
          numofdefItem=y;
          
          
   }
+	   public int product(int x, int y,int z) 
+       {
+         typesOffrutis=x;
+         numofdefItem=y;
+         
+         
+  }
+	  public double product(double x, double y) {
+		  typesOffrutis=x;
+         numofdefItem=y;
+         
+		  
+	  }
+	 }
   
-  public static void main(string args[])
+  public static void main(String args[])
   {
+		product p=new product(10,15);
+	  product p=new product(10,15,10);
+	  product p=new product(10d,15d);
+
+	  System.out.print(p);
+	  
+	  
+	  
   }
     
    
     }
-  }
-
-}
