fc90edb65faabcc7079ec6bcebd6b14f32d2421a
bluedrago3 <95340265+bluedrago3@users.noreply.github.com>
2023-02-15 14:41:28 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+  class product{
+    int typesOffrutis;
+    int numofdefectivefruits;
+    int numofdefItem;
+    
+     public int product(int x, int y) {
+       {
+         typesoffruits=x;
+         numofdefItem=y;
+         
+         
+  }
+  
+  public static void main(string args[])
+  {
+  }
+    
+   
+    }
+  }
 
 }
